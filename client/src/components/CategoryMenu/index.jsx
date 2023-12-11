3e5ad6e802f0c7f020c45cc670194b78b5aa4437
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-10 17:59:43 -0800

    Fix CategoryMenu/index.jsx

--- client/src/components/CategoryMenu/index.jsx
+++ client/src/components/CategoryMenu/index.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-// import { useStoreContext } from '../../utils/GlobalState';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   UPDATE_CATEGORIES,
@@ -10,11 +9,8 @@ import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
-  // const [state, dispatch] = useStoreContext();
   const dispatch = useDispatch();
-  const state = useSelector((state) => state);
-
-  const { categories } = state;
+  const categories = useSelector((state) => state.categories);
 
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
