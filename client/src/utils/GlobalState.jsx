8ae20cad41770920e36e3f2dbcd392034400f331
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-10 18:53:23 -0800

    Remove commented code in GlobalState.jsx

--- client/src/utils/GlobalState.jsx
+++ client/src/utils/GlobalState.jsx
@@ -1,29 +1,3 @@
-// import { createContext, useContext, useReducer } from "react";
-// import { reducer } from './reducers'
-
-// const StoreContext = createContext();
-// const { Provider } = StoreContext;
-
-// const StoreProvider = ({ value = [], ...props }) => {
-//   const [state, dispatch] = useReducer(reducer, {
-//     products: [],
-//     cart: [],
-//     cartOpen: false,
-//     categories: [],
-//     currentCategory: '',
-//   });
-
-//   return <Provider value={[state, dispatch]} {...props} />;
-// };
-
-// const useStoreContext = () => {
-//   return useContext(StoreContext);
-// };
-
-// export { StoreProvider, useStoreContext };
-
-//////////////////////////////////////////////////////NEW\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
 import { Provider } from 'react-redux';
 
 // Import Redux store from the file we created in /utils
@@ -33,5 +7,3 @@ export default function StoreProvider(props) {
   // Return the provider component provided by react-redux
   return <Provider store={store} {...props} />;
 }
-
-//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\NEW////////////////////////////////////////////////////\\
