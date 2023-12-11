dcd3904edbfb8aae3fc35625db65820cb0e34a14
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-10 17:35:12 -0800

    Update reducers.js

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -19,7 +19,7 @@ const initialState = {
 };
 
 // The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
-export const reducer = (state = initialState, action) => {
+export default function reducer(state = initialState, action) {
   switch (action.type) {
     // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
     case UPDATE_PRODUCTS:
@@ -95,7 +95,8 @@ export const reducer = (state = initialState, action) => {
 
     // Return the state as is in the event that the `action.type` passed to our reducer was not accounted for by the developers
     // This saves us from a crash.
-    default:
+    default: {
       return state;
+    }
   }
-};
+}
